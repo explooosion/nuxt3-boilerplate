57be5071960291af504ad54dfce25cae20b262a6
Robby <ta7382@gmail.com>
2023-07-08 14:56:45 +0800

    Initial configuration

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    {{ greet }}
     <NuxtWelcome />
   </div>
 </template>
+
+<script lang="ts">
+export default defineComponent({
+  setup() {
+    const greet = ref('Hello Nuxt3');
+
+    return {
+      greet
+    };
+  }
+});
+</script>
